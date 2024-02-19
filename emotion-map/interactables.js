93c398df4e080204bc76dcede44a85cd0e47a59e
branch <branch@pop-os.localdomain>
2024-02-19 23:48:08 +0400

    .

--- emotion-map/interactables.js
+++ emotion-map/interactables.js
@@ -2,20 +2,20 @@ var opacity_clicked = "1.0"
 var opacity_outed = "0.5"
 
 
-function CustomImage(img_path, img_width, opacity, input_id) {
+function CustomImage(img_path, img_width, opacity, input_id, pos_x, pos_y) {
 
     this.obj = createDiv()
 
     this.obj.img = createImg(img_path)
     this.obj.img.id(input_id)
     this.obj.img.style("width", img_width)
-    this.obj.img.position(0, 0)
+    this.obj.img.position(pos_x, pos_y)
     this.obj.img.style("width", img_width + "px")
     this.obj.img.style("opacity", opacity)
 }
 
 
-function Interface(pos_x, pos_y) {
+function Interface(pos_x, pos_y, offset_x, offset_y) {
 
     function emo_dropdown_changed() {
 
@@ -95,10 +95,18 @@ function Interface(pos_x, pos_y) {
                 alpha_ = "1.0"
             }
             let txt_id_ = text_dict[document.getElementById("txt_dropdown").value]
-            let wavepath = "model=" + model_ + "-" + "spk=" + spk_ + "-" + "emo=" + emo_ + "-" + "s=" + alpha_ + "-" + "txt=" + txt_id_ + ".wav"
+            let n_ = Math.floor(Math.random()*2) + 1
+            let wavepath = "model=" + model_ + "-" + "spk=" + spk_ + "-" + "emo=" + emo_ + "-" + "s=" + alpha_ + "-" + "txt=" + txt_id_ + "-" + "n=" + n_ + ".wav"
             wavepath = "assets/waves/custom_texts/" + wavepath
             document.getElementById("waveplayer").src = wavepath
-            console.log(wavepath)
+
+            // console.log(wavepath)
+            let wavepath_ = wavepath.split("/")[3]
+            let latent_coord = latent_coords[wavepath_]
+            // let x = latent_coord[0]/5 - 100
+            // let y = latent_coord[1]/5 + 100
+            // this.coord.position(x, y)
+            // console.log(x, y)
         }
     }
 
@@ -106,24 +114,25 @@ function Interface(pos_x, pos_y) {
 
     this.obj.emo_header = createP("Select Emotion: ")
     this.obj.emo_header.id("emo_header")
-    this.obj.emo_header.position(pos_x, pos_y)
+    this.obj.emo_header.position(pos_x-offset_x, pos_y-offset_y)
 
     this.obj.emo_dropdown_a = createSelect()
     this.obj.emo_dropdown_a.id("emo_dropdown_a")
-    this.obj.emo_dropdown_a.position(pos_x, pos_y+40)
+    this.obj.emo_dropdown_a.position(pos_x-offset_x, pos_y+40-offset_y)
     this.obj.emo_dropdown_a.option("-")
     this.obj.emo_dropdown_a.option("Sadness")
     this.obj.emo_dropdown_a.option("Joy")
     this.obj.emo_dropdown_a.option("Surprise")
     this.obj.emo_dropdown_a.option("Anger")
+    this.obj.emo_dropdown_a.selected("Sadness")
     this.obj.emo_dropdown_a.changed(emo_dropdown_changed)
 
     this.plus_sign = createP("<b>+<b>")
-    this.plus_sign.position(pos_x+95, pos_y+25)
+    this.plus_sign.position(pos_x+95-offset_x, pos_y+25-offset_y)
 
     this.obj.emo_dropdown_b = createSelect()
     this.obj.emo_dropdown_b.id("emo_dropdown_b")
-    this.obj.emo_dropdown_b.position(pos_x+120, pos_y+40)
+    this.obj.emo_dropdown_b.position(pos_x+120-offset_x, pos_y+40-offset_y)
     this.obj.emo_dropdown_b.option("-")
     this.obj.emo_dropdown_b.option("Sadness")
     this.obj.emo_dropdown_b.option("Joy")
@@ -132,53 +141,65 @@ function Interface(pos_x, pos_y) {
     this.obj.emo_dropdown_b.changed(emo_dropdown_changed)
     
 
-    this.obj.intensity_header = createP("Intensity: 1.25")
+    this.obj.intensity_header = createP("Intensity: 1")
     this.obj.intensity_header.id("intensity_header")
-    this.obj.intensity_header.position(pos_x, pos_y+70)
+    this.obj.intensity_header.position(pos_x-offset_x, pos_y+70-offset_y)
 
-    this.obj.intensity_slider = createSlider(0.25, 1.75, 1.25, 0.25)
+    this.obj.intensity_slider = createSlider(0.25, 1.75, 1, 0.75)
     this.obj.intensity_slider.id("intensity_slider")
-    this.obj.intensity_slider.position(pos_x, pos_y+110)
+    this.obj.intensity_slider.position(pos_x-offset_x, pos_y+110-offset_y)
     this.obj.intensity_slider.changed(intensity_slider_changed)
 
     this.obj.spk_header = createP("Select Speaker: ")
     this.obj.spk_header.id("spk_header")
-    this.obj.spk_header.position(pos_x, pos_y+145)
+    this.obj.spk_header.position(pos_x-offset_x, pos_y+145-offset_y)
 
     this.obj.spk_dropdown = createSelect()
     this.obj.spk_dropdown.id("spk_dropdown")
-    this.obj.spk_dropdown.position(pos_x+140, pos_y+160)
+    this.obj.spk_dropdown.position(pos_x+140-offset_x, pos_y+160-offset_y)
     this.obj.spk_dropdown.option("0016")
     this.obj.spk_dropdown.option("0013")
     this.obj.spk_dropdown.option("0019")
     this.obj.spk_dropdown.option("0020")
+    this.obj.spk_dropdown.selected("0016")
 
     this.obj.txt_header = createP("Select Text: ")
     this.obj.txt_header.id("txt_header")
-    this.obj.txt_header.position(pos_x, pos_y+180)
+    this.obj.txt_header.position(pos_x-offset_x, pos_y+180-offset_y)
 
     this.obj.txt_dropdown = createSelect()
     this.obj.txt_dropdown.id("txt_dropdown")
-    this.obj.txt_dropdown.position(pos_x+140, pos_y+195)
+    this.obj.txt_dropdown.position(pos_x+140-offset_x, pos_y+195-offset_y)
     this.obj.txt_dropdown.option("Oh my god, what are the chances.")
     this.obj.txt_dropdown.option("How do you know this.")
-    this.obj.txt_dropdown.option("You really did it again.")
-    this.obj.txt_dropdown.option("You are not attending the event.")
-    this.obj.txt_dropdown.option("He was so close to me.")
-    this.obj.txt_dropdown.option("We are running out of milk.")
-    this.obj.txt_dropdown.option("Let me tell you something.")
-    this.obj.txt_dropdown.option("That's exactly what happened.")
-    this.obj.txt_dropdown.option("Can you believe it.")
+    // this.obj.txt_dropdown.option("You really did it again.")
+    // this.obj.txt_dropdown.option("You are not attending the event.")
+    // this.obj.txt_dropdown.option("He was so close to me.")
+    // this.obj.txt_dropdown.option("We are running out of milk.")
+    // this.obj.txt_dropdown.option("Let me tell you something.")
+    // this.obj.txt_dropdown.option("That's exactly what happened.")
+    // this.obj.txt_dropdown.option("Can you believe it.")
     this.obj.txt_dropdown.option("Again, overall, not just for me.")
+    this.obj.txt_dropdown.selected("How do you know this.")
 
     this.obj.sample_button = createButton("Sample Emotion")
-    this.obj.sample_button.position(pos_x, pos_y+275)
+    this.obj.sample_button.position(pos_x-offset_x, pos_y+275-offset_y)
     this.obj.sample_button.mouseClicked(sample_emotion)
+
+    // this.obj.sample_button.coord = createDiv()
+    // this.obj.sample_button.coord.id("coord")
+    // this.obj.sample_button.coord.position(0, 0)
+    // this.obj.sample_button.coord.style("background-color", "#555")
+    // this.obj.sample_button.coord.style("height", "12.5px")
+    // this.obj.sample_button.coord.style("width", "12.5px")
+    // this.obj.sample_button.coord.style("border-radius", "50%")
 
     this.obj.waveplayer = createAudio("")
     this.obj.waveplayer.id("waveplayer")
-    this.obj.waveplayer.position(pos_x, pos_y+310)
+    this.obj.waveplayer.position(pos_x-offset_x, pos_y+310-offset_y)
     this.obj.waveplayer.showControls()
+
+    document.getElementById("waveplayer").src = "assets/waves/custom_texts/model=rainbow-spk=0016-emo=sadness-s=1.0-txt=1-n=2.wav"
 
 }
 
--- emotion-map/sketch.js
+++ emotion-map/sketch.js
@@ -2,7 +2,7 @@ let canvas_width = 1000
 let canvas_height = 800
 let canvas_color = [255,255,255]
 
-var img_width = 500
+var img_width = 600
 
 var CLICKED_COUNT = 0
 var emo_list = [
@@ -12,19 +12,19 @@ var emo_list = [
 var text_dict = {
     "Oh my god, what are the chances.": 1,
     "How do you know this.": 2,
-    "You really did it again.": 3,
-    "You are not attending the event.": 4,
-    "He was so close to me.": 5,
-    "We are running out of milk.": 6,
-    "Let me tell you something.": 7,
-    "That's exactly what happened.": 8,
-    "Can you believe it.": 9,
+    // "You really did it again.": 3,
+    // "You are not attending the event.": 4,
+    // "He was so close to me.": 5,
+    // "We are running out of milk.": 6,
+    // "Let me tell you something.": 7,
+    // "That's exactly what happened.": 8,
+    // "Can you believe it.": 9,
     "Again, overall, not just for me.": 10
 }
 var active_emo = "-"
 var ints2opac = {
     0.0: 0.25,
-    0.25: 0.70,
+    0.25: 0.75,
     0.5: 0.75,
     0.75: 0.80,
     1.00: 0.85,
@@ -33,81 +33,33 @@ var ints2opac = {
     1.75: 1.0,
 }
 
+function preload() {
+    latent_coords = loadJSON('assets/waves/custom_texts_v3_coords-px.json')
+}
 
 function setup() {
+
     createCanvas(canvas_width, canvas_height)
     background(color(canvas_color[0],canvas_color[1], canvas_color[2]))
 
+    let img_offset_x = -100
+    let img_offset_y = -85
 
-    CustomImage("assets/embedding-space/space-joy.png", img_width, ints2opac[0.0], "img_joy")
-    CustomImage("assets/embedding-space/space-sadness.png", img_width, ints2opac[0.0], "img_sadness")
-    CustomImage("assets/embedding-space/space-anger.png", img_width, ints2opac[0.0], "img_anger")
-    CustomImage("assets/embedding-space/space-surprise.png", img_width, ints2opac[0.0], "img_surprise")
+    let offset_y = 80
+    let offset_x = 60
+
+    CustomImage("assets/embedding-space/space-joy.png", img_width, ints2opac[0.0], "img_joy", img_offset_x, img_offset_y)
+    CustomImage("assets/embedding-space/space-sadness.png", img_width, ints2opac[0.0], "img_sadness", img_offset_x, img_offset_y)
+    CustomImage("assets/embedding-space/space-anger.png", img_width, ints2opac[0.0], "img_anger", img_offset_x, img_offset_y)
+    CustomImage("assets/embedding-space/space-surprise.png", img_width, ints2opac[0.0], "img_surprise", img_offset_x, img_offset_y)
     
-    CustomImage("assets/embedding-space/space-bittersweetness.png", img_width, ints2opac[0.0], "img_bittersweetness")
-    CustomImage("assets/embedding-space/space-delight.png", img_width, ints2opac[0.0], "img_delight")
-    CustomImage("assets/embedding-space/space-envy.png", img_width, ints2opac[0.0], "img_envy")
-    CustomImage("assets/embedding-space/space-pride.png", img_width, ints2opac[0.0], "img_pride")
-    CustomImage("assets/embedding-space/space-outrage.png", img_width, ints2opac[0.0], "img_outrage")
-    CustomImage("assets/embedding-space/space-disappointment.png", img_width, ints2opac[0.0], "img_disappointment")
+    CustomImage("assets/embedding-space/space-bittersweetness.png", img_width, ints2opac[0.0], "img_bittersweetness", img_offset_x, img_offset_y)
+    CustomImage("assets/embedding-space/space-delight.png", img_width, ints2opac[0.0], "img_delight", img_offset_x, img_offset_y)
+    CustomImage("assets/embedding-space/space-envy.png", img_width, ints2opac[0.0], "img_envy", img_offset_x, img_offset_y)
+    CustomImage("assets/embedding-space/space-pride.png", img_width, ints2opac[0.0], "img_pride", img_offset_x, img_offset_y)
+    CustomImage("assets/embedding-space/space-outrage.png", img_width, ints2opac[0.0], "img_outrage", img_offset_x, img_offset_y)
+    CustomImage("assets/embedding-space/space-disappointment.png", img_width, ints2opac[0.0], "img_disappointment", img_offset_x, img_offset_y)
 
-    Interface(canvas_width/2, canvas_height/8)
+    Interface(canvas_width/2, canvas_height/8, offset_x, offset_y)
 
-    // InteractiveImage("assets/embedding-space/space-sadness.png", 500, 150, 325, "sadness")
-    // InteractiveImage("assets/embedding-space/space-anger.png", 500, 300, 320, "anger")
-    // InteractiveImage("assets/embedding-space/space-surprise.png", 500, 180, 150, "surprise")
-
-    // InteractiveImage("assets/embedding-space/space-bittersweetness.png", 500, 160, 260, "bittersweetness")
-    // InteractiveImage("assets/embedding-space/space-delight.png", 500, 260, 130, "delight")
-    // InteractiveImage("assets/embedding-space/space-envy.png", 500, 240, 350, "envy")
-    // InteractiveImage("assets/embedding-space/space-pride.png", 500, 350, 275, "pride")
-    // InteractiveImage("assets/embedding-space/space-outrage.png", 500, 310, 225, "outrage")
-    // InteractiveImage("assets/embedding-space/space-disappointment.png", 500, 140, 200, "disappointment")
-
-    // InteractiveText(500, 110)
 }
-
-// function draw() {
-
-//     // Intensity indicator
-//     //document.getElementById("intensity_info").intensity_info.innerHTML = "Intensity: " + document.getElementById("intensity_slider").intensity_slider.value()
-
-//     // Emotion indicator
-//     if (ACTIVE_EMOS["joy"] == 1) {
-//         document.getElementById("emo_info").innerHTML = "Emotion: Joy"
-//     }
-//     else if (ACTIVE_EMOS["sadness"] == 1) {
-//         document.getElementById("emo_info").innerHTML = "Emotion: Sadness"
-//     }
-//     else if (ACTIVE_EMOS["surprise"] == 1) {
-//         document.getElementById("emo_info").innerHTML = "Emotion: Surprise"
-//     }
-//     else if (ACTIVE_EMOS["anger"] == 1) {
-//         document.getElementById("emo_info").innerHTML = "Emotion: Anger"
-//     }
-
-//     else if (ACTIVE_EMOS["bittersweetness"] == 1) {
-//         document.getElementById("emo_info").innerHTML = "Emotion: Bittersweetness"
-//     }
-//     else if (ACTIVE_EMOS["delight"] == 1) {
-//         document.getElementById("emo_info").innerHTML = "Emotion: Delight"
-//     }
-//     else if (ACTIVE_EMOS["envy"] == 1) {
-//         document.getElementById("emo_info").innerHTML = "Emotion: Envy"
-//     }
-//     else if (ACTIVE_EMOS["pride"] == 1) {
-//         document.getElementById("emo_info").innerHTML = "Emotion: Pride"
-//     }
-//     else if (ACTIVE_EMOS["outrage"] == 1) {
-//         document.getElementById("emo_info").innerHTML = "Emotion: Outrage"
-//     }
-//     else if (ACTIVE_EMOS["disappointment"] == 1) {
-//         document.getElementById("emo_info").innerHTML = "Emotion: Disappointment"
-//     }
-
-//     else {
-//         document.getElementById("emo_info").innerHTML = "Emotion: "
-//     }
-    
-
-// }
